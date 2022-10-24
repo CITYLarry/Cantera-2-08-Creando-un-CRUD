87f1d178fff8bd1168e00886a2968f0d5f04ece4
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-10-24 16:19:15 -0500

    Create the project

---
+++ Sistema DEMO de Contactos/src/main/java/com/sofka/contactos/SistemaDemoDeContactosApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.contactos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDemoDeContactosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDemoDeContactosApplication.class, args);
+	}
+
+}
---
+++ Sistema DEMO de Contactos/src/test/java/com/sofka/contactos/SistemaDemoDeContactosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.contactos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDemoDeContactosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
