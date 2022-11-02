bea354dbc4a212e1fa32aa9ccee90ac39fc06bb6
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-02 14:45:38 -0500

    Create the project Address Book

---
+++ Adress Book/src/test/java/com/sofka/adressbook/AdressBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.adressbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdressBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
