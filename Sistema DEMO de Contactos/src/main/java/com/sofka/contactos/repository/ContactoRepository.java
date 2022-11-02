b206e0cb7a6eabaea4fd71a77ac32658450c6792
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-01 20:25:31 -0500

    Update ContactoRepository.java
    Add a method findContacto, that allows to find a contact by its name or last name in a database.
    Also add the methods updateFirstName and updateLastname that allows update the name and last name from a contact respectively.

--- Sistema DEMO de Contactos/src/main/java/com/sofka/contactos/repository/ContactoRepository.java
+++ Sistema DEMO de Contactos/src/main/java/com/sofka/contactos/repository/ContactoRepository.java
@@ -2,6 +2,11 @@ package com.sofka.contactos.repository;
 
 import com.sofka.contactos.domain.Contacto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 /**
  * Repository for Contacto entity
@@ -10,4 +15,23 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Larry Mateo Ramirez C.
  */
 public interface ContactoRepository extends JpaRepository<Contacto, Integer> {
+
+    @Query(value = "SELECT c.id " +
+            "FROM Contacto  c " +
+            "WHERE c.cntNombre LIKE %:data% OR c.cntApellido LIKE %:data% " +
+            "ORDER BY c.cntNombre ASC")
+    public List<Contacto> findContacto(@Param("data") String data);
+
+    @Modifying
+    @Query(value = "UPDATE Contacto c " +
+            "SET c.cntNombre = :name, c.cntUpdatedAt = CURRENT_TIMESTAMP " +
+            "WHERE c.id= :id")
+    public void updateFirstName(@Param(value = "id") Integer id, @Param(value = "name") String name);
+
+    @Modifying
+    @Query(value = "UPDATE Contacto c " +
+            "SET c.cntApellido = :lastname, c.cntUpdatedAt = CURRENT_TIMESTAMP " +
+            "WHERE c.id = :id")
+    public void updateLastname(@Param(value = "id") Integer id, @Param(value = "lastname") String name);
+
 }
