f0385ebfad9908b2c9fd4a80d0c5ef518c7adbc6
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-01 20:26:02 -0500

    Update TelefonoRepository.java
    Add the method updateTelefono that allows update the telephone number of a contact.

--- Sistema DEMO de Contactos/src/main/java/com/sofka/contactos/repository/TelefonoRepository.java
+++ Sistema DEMO de Contactos/src/main/java/com/sofka/contactos/repository/TelefonoRepository.java
@@ -2,6 +2,9 @@ package com.sofka.contactos.repository;
 
 import com.sofka.contactos.domain.Telefono;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /**
  * Repository for Telefono entity
@@ -10,4 +13,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * @author Larry Mateo Ramirez C.
  */
 public interface TelefonoRepository extends JpaRepository<Telefono, Integer> {
+
+    @Modifying
+    @Query(value = "UPDATE Telefono t " +
+            "SET t.telTelefono = :telephone, t.telUpdatedAt = CURRENT_TIMESTAMP " +
+            "WHERE t.id = :id")
+    public void updateTelefono(@Param(value = "id") Integer id, @Param(value = "telephone") String telephone);
+
 }
