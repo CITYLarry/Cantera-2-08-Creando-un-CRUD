e603145853ff6331b2b3b52dd5aa7b0d351cf6fe
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-03 09:45:22 -0500

    Creates the spring project Address Book

---
+++ Address Book/src/main/java/com/sofka/addressbook/AddressBookApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddressBookApplication.class, args);
+    }
+
+}
