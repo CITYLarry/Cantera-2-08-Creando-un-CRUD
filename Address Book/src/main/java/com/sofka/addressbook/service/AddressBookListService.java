575968cf7623cb2c30bdc390572fffd2f3e995c2
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-03 11:45:11 -0500

    Create AddressBookListService.java
    Create an define the service for the address book

---
+++ Address Book/src/main/java/com/sofka/addressbook/service/AddressBookListService.java
@@ -0,0 +1,151 @@
+package com.sofka.addressbook.service;
+
+import com.sofka.addressbook.domain.AddressBookList;
+import com.sofka.addressbook.repository.AddressBookListRepository;
+import com.sofka.addressbook.service.interfaces.IAddressBookList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+
+/**
+ * Service implementation for the contact list AddressBookList
+ *
+ * @version 1.0.0 2022-11-03
+ * @author Larry Mateo Ramirez C.
+ */
+@Service
+public class AddressBookListService implements IAddressBookList{
+
+    @Autowired
+    private AddressBookListRepository addressBookListRepository;
+
+    /**
+     * Return a list object that contains all the contacts in the database
+     *
+     * @return List of contacts
+     */
+    @Override
+    @Transactional(readOnly = true)
+    public List<AddressBookList> getList() {
+        return addressBookListRepository.findAll();
+    }
+
+    /**
+     * Return a list object that contains all the contacts with the coincident filter
+     *
+     * @param data Searching filter
+     * @return     List of contacts
+     */
+    @Override
+    public List<AddressBookList> search(String data) {
+        return addressBookListRepository.search(data);
+    }
+
+    /**
+     * Creates a new contact
+     *
+     * @param contact Object that contains the contact to be created
+     * @return        AddressBookList object created
+     */
+    @Override
+    public AddressBookList newContact(AddressBookList contact) {
+        contact.setAbCreatedAt(Instant.now());
+        addressBookListRepository.save(contact);
+        return contact;
+    }
+
+    /**
+     * Updates a contact
+     *
+     * @param id      Contact ID to update
+     * @param contact Object that contains the contact to be updated
+     * @return AddressBookList object updated
+     */
+    @Override
+    public AddressBookList updateContact(Integer id, AddressBookList contact) {
+        contact.setId(id);
+        contact.setAbUpdatedAt(Instant.now());
+        addressBookListRepository.save(contact);
+        return contact;
+    }
+
+    /**
+     * Updates a contact name
+     *
+     * @param id      Contact ID to update
+     * @param contact Object that contains the contact to be updated
+     * @return AddressBookList object updated
+     */
+    @Override
+    public AddressBookList updateName(Integer id, AddressBookList contact) {
+        contact.setId(id);
+        contact.setAbUpdatedAt(Instant.now());
+        addressBookListRepository.updateName(id, contact.getAbName());
+        return contact;
+    }
+
+    /**
+     * Updates a contact phone number
+     *
+     * @param id      Contact ID to update
+     * @param contact Object that contains the contact to be updated
+     * @return AddressBookList object updated
+     */
+    @Override
+    public AddressBookList updatePhone(Integer id, AddressBookList contact) {
+        contact.setId(id);
+        contact.setAbUpdatedAt(Instant.now());
+        addressBookListRepository.updatePhone(id, contact.getAbPhone());
+        return contact;
+    }
+
+    /**
+     * Updates a contact email
+     *
+     * @param id      Contact ID to update
+     * @param contact Object that contains the contact to be updated
+     * @return AddressBookList object updated
+     */
+    @Override
+    public AddressBookList updateMail(Integer id, AddressBookList contact) {
+        contact.setId(id);
+        contact.setAbUpdatedAt(Instant.now());
+        addressBookListRepository.updateMail(id, contact.getAbMail());
+        return contact;
+    }
+
+    /**
+     * Logic delete of a contact
+     *
+     * @param id      Contact ID to update
+     * @param contact Object that contains the contact to be updated
+     * @return AddressBookList object updated
+     */
+    @Override
+    public AddressBookList updateDelete(Integer id, AddressBookList contact) {
+        contact.setId(id);
+        contact.setAbUpdatedAt(Instant.now());
+        contact.setAbDeletedAt(Instant.now());
+        addressBookListRepository.updateDelete(id);
+        return contact;
+    }
+
+    /**
+     * Deletes a contact
+     *
+     * @param id Contact ID to delete
+     * @return AddressBookList object deleted
+     */
+    @Override
+    public AddressBookList deleteContact(Integer id) {
+        var contact = addressBookListRepository.findById(id);
+        if(contact.isEmpty()) {
+            return null;
+        }
+        addressBookListRepository.delete(contact.get());
+        return contact.get();
+    }
+}
