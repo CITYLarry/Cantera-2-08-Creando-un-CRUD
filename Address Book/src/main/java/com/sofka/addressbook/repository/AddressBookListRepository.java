5caa90b765dc678858814316fc7f34b452d974f3
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-03 11:41:54 -0500

    Update AddressBookListRepository.java
    Add methods that allows to updates the contact parameters in the database.

--- Address Book/src/main/java/com/sofka/addressbook/repository/AddressBookListRepository.java
+++ Address Book/src/main/java/com/sofka/addressbook/repository/AddressBookListRepository.java
@@ -2,6 +2,7 @@ package com.sofka.addressbook.repository;
 
 import com.sofka.addressbook.domain.AddressBookList;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,9 +16,62 @@ import java.util.List;
  */
 public interface AddressBookListRepository extends JpaRepository<AddressBookList, Integer> {
 
+    /**
+     * Return a lis of contacts that contains the searching filter in its name
+     *
+     * @param data Data to search
+     * @return     List of contacts that contain the search filter
+     */
     @Query(value = "SELECT a.id " +
             "FROM AddressBookList a " +
             "WHERE a.abName LIKE %:data% " +
             "ORDER BY a.abName ASC")
     public List<AddressBookList> search(@Param("data") String data);
+
+    /**
+     * Updates the contact name in the database
+     *
+     * @param id   Contact ID to update
+     * @param name Name to update
+     */
+    @Modifying
+    @Query(value = "UPDATE AddressBookList a " +
+            "SET a.abName = :name, a.abUpdatedAt = CURRENT_TIMESTAMP " +
+            "WHERE a.id = :id")
+    public void updateName(@Param(value = "id") Integer id, @Param(value = "name") String name);
+
+    /**
+     * Updates the contact phone number in the database
+     *
+     * @param id    Contact ID to update
+     * @param phone Phone number to update
+     */
+    @Modifying
+    @Query(value = "UPDATE AddressBookList a " +
+            "SET a.abPhone = :phone, a.abUpdatedAt = CURRENT_TIMESTAMP " +
+            "WHERE a.id = :id")
+    public void updatePhone(@Param(value = "id") Integer id, @Param(value = "phone") String phone);
+
+    /**
+     * Updates the contact mail in the database
+     *
+     * @param id   Contact ID to update
+     * @param mail Mail to update
+     */
+    @Modifying
+    @Query(value = "UPDATE AddressBookList a " +
+            "SET a.abMail = :mail, a.abUpdatedAt = CURRENT_TIMESTAMP " +
+            "WHERE a.id = :id")
+    public void updateMail(@Param(value = "id") Integer id, @Param(value = "mail") String mail);
+
+    /**
+     * Updates the contact to be deleted in the database
+     *
+     * @param id Contact ID to delete
+     */
+    @Modifying
+    @Query(value = "UPDATE AddressBookList a " +
+            "SET a.abDeletedAt = CURRENT_TIMESTAMP " +
+            "WHERE a.id = :id")
+    public void updateDelete(@Param(value = "id") Integer id);
 }
