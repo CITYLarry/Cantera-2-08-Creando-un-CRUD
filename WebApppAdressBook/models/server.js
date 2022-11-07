b104ee144f7aa544347af4b9d653e253105291eb
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-06 19:09:21 -0500

    Update server.js
    Server config

--- WebApppAdressBook/models/server.js
+++ WebApppAdressBook/models/server.js
@@ -1,9 +1,15 @@
 import express from "express";
-import bodyParser from "body-parser";
 import {router as routerIndex} from "../routes/index.js";
+import {router as routerEdit} from "../routes/edit.js"
 import "hbs";
 
+/**
+ * Server definition of the web application
+ */
 export default class Server {
+    /**
+     * constructor
+     */
     constructor() {
         this.app = express();
         this.port = process.env.PORT || 3000;
@@ -19,23 +25,36 @@ export default class Server {
 
     }
 
+    /**
+     * Define the middlewares used by the app
+     */
     middlewares() {
         // json encoder
-        this.app.use(bodyParser.json());
+        this.app.use(express.json());
         // body parser
-        this.app.use(bodyParser.urlencoded({extended: true}));
+        this.app.use(express.urlencoded({extended: true}));
         // public directory
         this.app.use(express.static("public"));
     }
 
+    /**
+     * Defines handdle bars as the view motor
+     */
     setview() {
         this.app.set("view-engine", "hbs");
     }
 
+    /**
+     * Defines the routes used
+     */
     routes() {
         this.app.use("/", routerIndex );
+        this.app.use("/edit", routerEdit);
     }
 
+    /**
+     * Stars the server instance
+     */
     listen() {
         this.app.listen(this.port, () => {
             console.log(`Server on port ${this.port}`);
