b1abb868b6248e40d7397e417e5e1d640cdf4b96
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-06 19:08:14 -0500

    Update index.js
    Routes for index

--- WebApppAdressBook/routes/index.js
+++ WebApppAdressBook/routes/index.js
@@ -1,6 +1,23 @@
 import { Router } from "express";
-import {renderIndex} from "../controllers/index.js";
+import { renderIndex, postIndex, deleteIndex } from "../controllers/index.js";
 
+/**
+ * Instance of the element Router
+ */
 export const router = Router();
 
+/**
+ * Handle the get method for the index
+ */
 router.get("/", renderIndex);
+
+/**
+ * Handle de post method fo the index
+ */
+router.post("/", postIndex);
+
+/**
+ * Handle the redirection method for a delete CRUD action
+ */
+router.get("/delete/:id", deleteIndex);
+
