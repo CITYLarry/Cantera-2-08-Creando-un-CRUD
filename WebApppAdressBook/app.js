ca29063e9dc43cb78acdeb1ed6ca472405bb8031
Larry Mateo Ramirez C <larry.ramirez@correounivalle.edu.co>
2022-11-06 19:09:49 -0500

    Update app.js
    App documentation

--- WebApppAdressBook/app.js
+++ WebApppAdressBook/app.js
@@ -1,6 +1,13 @@
 import {} from "dotenv/config";
 
 import Server from "./models/server.js";
+
+/**
+ * Intsance of the clas Server
+ */
 const server = new Server();
 
+/**
+ * Server initialization
+ */
 server.listen();
